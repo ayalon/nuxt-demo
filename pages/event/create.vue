4d2aaa69bbe2bdd4b82905f32d18e917f6728750
ayalon GmbH <info@ayalon.ch>
2020-06-21 11:47:37 +0200

    Initial commit

---
+++ pages/event/create.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h1>Create an Events</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  head() {
+    return {
+      title: 'Create an Event',
+      meta: [
+        {
+          hid: 'description',
+          name: 'description',
+          content: 'Create and Event.',
+        },
+      ],
+    }
+  },
+}
+</script>
+
+<style scoped></style>
